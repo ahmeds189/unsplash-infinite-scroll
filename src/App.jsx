1c56c793b70cbc58311a19e1f2baa19e6242c59c
ahmed <primaryahmedsaid@gmail.com>
2023-01-28 16:54:00 +0200

    initial app

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect, useRef } from "react";
+
+import Photo from "./Photo";
+const clientID = `?client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [newImags, setNewImgs] = useState(false);
+  const mounted = useRef(false);
+
+  const fetchImags = async () => {
+    setLoading(true);
+    let url;
+    const pageUrl = `&page=${page}`;
+    const pageQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${pageUrl}${pageQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${pageUrl}`;
+    }
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImgs(false);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImags();
+    // eslint-disable-next-line
+  }, [page]);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImags) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 1);
+    // eslint-disable-next-line
+  }, [newImags]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImgs(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImags();
+      return;
+    }
+    setPage(1);
+  };
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-search"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+              <path d="M21 21l-6 -6"></path>
+            </svg>
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo, index) => {
+            return <Photo key={index} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
+};
+
+export default App;
